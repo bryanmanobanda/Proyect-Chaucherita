7fa2433b13b6fe5423091e0e61bf501587bbc755
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-02-11 22:25:19 -0500

    Clase Cuenta

--- src/main/java/modelo/Cuenta.java
+++ src/main/java/modelo/Cuenta.java
@@ -1,5 +1,63 @@
 package modelo;
 
-public class Cuenta {
-	private int id;
+public abstract class Cuenta {
+	protected int id;
+	protected String nombre;
+	protected String descripcion;
+
+	public Cuenta() {
+
+	}
+	
+	public Cuenta(int id, String nombre, String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
+	
+	
 }
