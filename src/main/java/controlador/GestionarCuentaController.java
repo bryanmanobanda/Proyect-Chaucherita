47e7e9c8db803802d709053abe617a8ea5d61efe
Bryan Steven Manobanda <bryan.sst@hotmail.com>
2023-02-11 23:29:04 -0500

    Creacion JSP y Controller

---
+++ src/main/java/controlador/GestionarCuentaController.java
@@ -0,0 +1,79 @@
+package controlador;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GestionarCuentaController
+ */
+@WebServlet("/GestionarCuentaController")
+public class GestionarCuentaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GestionarCuentaController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			procesar(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			procesar(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void procesar(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		String ruta = (request.getParameter("ruta") == null ? "listar":request.getParameter("ruta"));
+		switch(ruta) {
+		case "listar":
+			this.listar(request, response);
+			break;
+		/*case "nuevo":
+			this.nuevo(request, response);
+			break;
+		case "guardar":
+			this.guardar(request, response);
+			break;
+		case "eliminar":
+			this.eliminar(request, response);
+			break;
+		case "actualizar":
+			this.actualizar(request, response);
+			break;
+		case "guardarA":
+			this.actualizarDato(request, response);
+			break;*/
+		}
+	}
+	
+	private void listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+		request.getRequestDispatcher("jsp/PanelPrincipal.jsp").forward(request,response);
+	}
+
+}
---
+++ src/main/webapp/jsp/movimiento.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix='c' uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Movimientos</title>
+</head>
+<body>
+	<h1>Traspasar dinero entre cuentas</h1>
+	<label for="cuentaOrigen">Cuenta origen</label>
+	<select name="cuentaOrigen" id="cuentaOrigen">
+	  <option value="Cuenta1">Cuenta 1</option>
+	  <option value="saab">Saab</option>
+	  <option value="mercedes">Mercedes</option>
+	  <option value="audi">Audi</option>
+	</select>
+	<label for="cuentaDestino">Cuenta Destino</label>
+	<select name="cuentaDestino" id="cuentaDestino">
+	  <option value="Cuenta1">Cuenta 1</option>
+	  <option value="saab">Saab</option>
+	  <option value="mercedes">Mercedes</option>
+	  <option value="audi">Audi</option>
+	</select>
+	<label for="monto">Agregar Monto</label>
+	<input type="text">
+	
+</body>
+</html>
